3e76e879a9458c0565c99e5591acb76d85016145
Osama <osama1992n49@gmail.com>
2026-06-02 02:10:22 +0300

    first commit

---
+++ ChatApp/API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities
+{
+    public class AppUser
+    {
+        public string Id { get; set; }
+        public required string DisplayName { get; set; }
+        public required string Email { get; set; }
+    }
+}
