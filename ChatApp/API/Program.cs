524829bb6f082af985dedabd12e5064ecbe7afdc
Osama <osama1992n49@gmail.com>
2026-06-03 18:55:52 +0300

    الانتهاء من انشاء مشروعين الbackend و ال frontend

--- ChatApp/API/Program.cs
+++ ChatApp/API/Program.cs
@@ -4,18 +4,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
-            var app = builder.Build();
+builder.Services.AddCors();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
-            app.Run();
+app.Run();
 
