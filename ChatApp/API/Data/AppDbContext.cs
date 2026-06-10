42ada6e495ab987362af5a0f7f066a050b43ec69
Osama <osama1992n49@gmail.com>
2026-06-11 01:01:03 +0300

    ربطنا العلاقات باستخدام EF وانشانا Seeding Data واستخدمنا Repository Pattern

--- ChatApp/API/Data/AppDbContext.cs
+++ ChatApp/API/Data/AppDbContext.cs
@@ -8,5 +8,7 @@ namespace API.Data
     public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
